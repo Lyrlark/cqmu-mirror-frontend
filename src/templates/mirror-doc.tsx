9f0f605b5c43eeb7b0b89ce9badebb86c3935fe1
zyh001 <stackzhao@gmail.com>
2024-06-04 11:51:27 +0800

    删除不用的圆括号

--- src/templates/mirror-doc.tsx
+++ src/templates/mirror-doc.tsx
@@ -66,125 +66,119 @@ const MirrorDoc = ({ data, children }: PropsWithChildren<MirrorDocProps>) => {
   const mirrorUrl = getUrl(mirror.url ?? fallbackUrl, false);
 
   const name = mirror.name[language as Locale];
-  return (
-    <Box
-      sx={{
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}
-    >
-      <Seo title={`${name} | JCUT Mirror`} />
-      <Box>
-        <Box sx={{ px: { xs: 4, sm: 8 }, py: 4 }} position="relative">
-          <Grid
-            container
-            direction="column"
-            spacing={4}
-            justifyContent="flex-start"
-            alignItems="flex-start"
-          >
-            <Grid item sx={{ width: '100%' }}>
-              <Grid container justifyContent="space-between">
-                <Link color="primary" underline="hover" to="/">
-                  <Typography variant="h5" component="div" color="primary">
-                    <Trans>JCUT Mirror</Trans>
-                  </Typography>
-                </Link>
-                <Grid item>
-                  {/* TODO: add English docs
-                    <LanguageIconButton />
-                    */}
-                  <ThemeIconButton />
-                </Grid>
-              </Grid>
-              <Box
-                sx={{
-                  mt: 4,
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}
-              />
-
-              <Typography variant="h2" fontWeight={400} component="div">
-                {name}
-              </Typography>
-
-              <Grid
-                container
-                direction="row"
-                justifyContent="flex-start"
-                alignItems="center"
-              >
-                <Box sx={{ mr: 1 }}>
-                  <StatusIndicator status={mirror.status} />
-                </Box>
-                <Typography
-                  variant="subtitle1"
-                  component="div"
-                  color="text.disabled"
-                  display={mirror.status === 'cached' ? 'none' : 'block'}
-                >
-                  <Trans>
-                    最近更新于{' '}
-                    {{
-                      date: new Date(mirror.lastUpdated * 1000).toLocaleString(
-                        language
-                      ),
-                    }}
-                  </Trans>
+  return <Box
+    sx={{
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}
+  >
+    <Seo title={`${name} | JCUT Mirror`} />
+    <Box>
+      <Box sx={{ px: { xs: 4, sm: 8 }, py: 4 }} position="relative">
+        <Grid
+          container
+          direction="column"
+          spacing={4}
+          justifyContent="flex-start"
+          alignItems="flex-start"
+        >
+          <Grid item sx={{ width: '100%' }}>
+            <Grid container justifyContent="space-between">
+              <Link color="primary" underline="hover" to="/">
+                <Typography variant="h5" component="div" color="primary">
+                  <Trans>JCUT Mirror</Trans>
                 </Typography>
+              </Link>
+              <Grid item>
+                {/* TODO: add English docs
+                  <LanguageIconButton />
+                  */}
+                <ThemeIconButton />
               </Grid>
             </Grid>
+            <Box
+              sx={{
+                mt: 4,
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}
+            />
 
-            {(mirror.files?.length ?? 0) > 0 && (
-              <Grid item width="100%">
-                <Box>
-                  <Typography gutterBottom variant="h5" component="div">
-                    <Trans>安装映像</Trans>
-                  </Typography>
+            <Typography variant="h2" fontWeight={400} component="div">
+              {name}
+            </Typography>
 
-                  <FileList files={mirror.files || []} />
-                </Box>
-              </Grid>
-            )}
-
-            {!data.document.frontmatter?.isGit && (
-              <Grid item>
-                <Button
-                  color="primary"
-                  size="medium"
-                  variant="contained"
-                  startIcon={<FolderIcon />}
-                  to={mirrorUrl}
-                >
-                  <Trans>文件列表</Trans>
-                </Button>
-              </Grid>
-            )}
+            <Grid
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <Box sx={{ mr: 1 }}>
+                <StatusIndicator status={mirror.status} />
+              </Box>
+              <Typography
+                variant="subtitle1"
+                component="div"
+                color="text.disabled"
+                display={mirror.status === 'cached' ? 'none' : 'block'}
+              >
+                <Trans>
+                  最近更新于{' '}
+                  {{
+                    date: new Date(mirror.lastUpdated * 1000).toLocaleString(
+                      language
+                    ),
+                  }}
+                </Trans>
+              </Typography>
+            </Grid>
           </Grid>
-          <Box
-            sx={{
-              position: 'fixed',
-              top: '6rem',
-              right: { xs: '-4rem', sm: '5rem' },
-              zIndex: -1,
-            }}
-          >
-            {TitleMirrorIcon(mirrorId, 'rgba(6,78,119,0.23)', '20rem')}
-          </Box>
-        </Box>
-        <Box zIndex={1} position="sticky">
-          <Paper sx={{ px: { xs: 4, sm: 8 }, py: 4 }} elevation={0}>
-            <MDXProvider components={components}>{children}</MDXProvider>
-          </Paper>
+
+          {(mirror.files?.length ?? 0) > 0 && <Grid item width="100%">
+              <Box>
+                <Typography gutterBottom variant="h5" component="div">
+                  <Trans>安装映像</Trans>
+                </Typography>
+
+                <FileList files={mirror.files || []} />
+              </Box>
+            </Grid>}
+
+          {!data.document.frontmatter?.isGit && <Grid item>
+              <Button
+                color="primary"
+                size="medium"
+                variant="contained"
+                startIcon={<FolderIcon />}
+                to={mirrorUrl}
+              >
+                <Trans>文件列表</Trans>
+              </Button>
+            </Grid>}
+        </Grid>
+        <Box
+          sx={{
+            position: 'fixed',
+            top: '6rem',
+            right: { xs: '-4rem', sm: '5rem' },
+            zIndex: -1,
+          }}
+        >
+          {TitleMirrorIcon(mirrorId, 'rgba(6,78,119,0.23)', '20rem')}
         </Box>
       </Box>
-      <Footer />
+      <Box zIndex={1} position="sticky">
+        <Paper sx={{ px: { xs: 4, sm: 8 }, py: 4 }} elevation={0}>
+          <MDXProvider components={components}>{children}</MDXProvider>
+        </Paper>
+      </Box>
     </Box>
-  );
+    <Footer />
+  </Box>;
 };
 
 export const query = graphql`

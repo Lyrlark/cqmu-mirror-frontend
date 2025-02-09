12b28a0a78e9dd272576b155cea394a3025a52ed
Los Desktop <losmosga@foxmail.com>
2025-02-10 06:39:18 +0800

    feat: add new frequently-used-mirror-list

--- src/utils/frequently-used-mirror-list.tsx
+++ src/utils/frequently-used-mirror-list.tsx
@@ -5,6 +5,8 @@ import archlinuxIcon from '@iconify/icons-logos/archlinux';
 import debianIcon from '@iconify/icons-logos/debian';
 import rockyIcon from '@iconify/icons-simple-icons/rockylinux';
 import centOsIcon from '@iconify/icons-logos/centos-icon';
+import pythonIcon from '@iconify/icons-simple-icons/python';
+import kaliIcon from '@iconify/icons-simple-icons/kalilinux';
 
 export default [
   {
@@ -26,5 +28,13 @@ export default [
   {
     id: 'rocky',
     icon: <Icon height="4rem" icon={rockyIcon} color="#10B981" />,
+  },
+  {
+    id: 'kali',
+    icon: <Icon height="4rem" icon={kaliIcon} color="#557C94" />,
+  },
+  {
+    id: 'python',
+    icon: <Icon height="4rem" icon={pythonIcon} color="#3776AB" />,
   },
 ];
